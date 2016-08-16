11bf1a6fa6fc351fab32e7352e20e5795909272b
Paolo Smiraglia <paolo.smiraglia@gmail.com>
2016-08-17 00:00:16 +0200

    Added commons module

---
+++ troposphereflink/commons.py
@@ -0,0 +1,35 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2016 Paolo Smiraglia
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+FLINK_INTERNET_GATEWAY = "FlinkInternetGateway"
+FLINK_NAT = "FlinkNATInstance"
+FLINK_NAT_SECURITY_GROUP = "FlinkNATSecurityGroup"
+FLINK_PRIVATE_ROUTE_TABLE = "FlinkPrivateRouteTable"
+FLINK_PRIVATE_SUBNET = "FlinkPrivateSubnet"
+FLINK_PUBLIC_ROUTE_TABLE = "FlinkPublicRouteTable"
+FLINK_PUBLIC_SUBNET = "FlinkPublicSubnet"
+FLINK_TEMPLATE_DESCRIPTION = "Composes a Flink cluster on AWS"
+FLINK_TEMPLATE_VERSION = "2010-09-09"
+FLINK_VPC = "FlinkVpc"
+FLINK_VPC_GATEWAY_ATTACHMENT = "FlinkVpcAttachmentGateway"
